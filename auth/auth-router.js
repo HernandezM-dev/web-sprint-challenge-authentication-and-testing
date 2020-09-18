85165bde1f44b0cc028ffcf09dc60a3b667e756c
HernandezM-dev <HernandezM.dev@gmail.com>
2020-09-18 11:56:04 -0400

    created users router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,57 +1,87 @@
-const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
 
 router.post('/register', (req, res) => {
-  const userInfo = req.body;
-  const isValid = validateUser(userInfo);
+  const credentials = req.body;
 
-  if (isValid) {
-      // hash the password before saving the user to the database
-      const rounds = process.env.BCRYPT_ROUNDS || 4;
-      const hash = bcryptjs.hashSync(userInfo.password, rounds);
-      userInfo.password = hash;
+  if (isValid(credentials)) {
+      const rounds = process.env.BCRYPT_ROUNDS || 8;
 
-      Users.add(userInfo)
-          .then(inserted => {
-              res.status(201).json({ data: inserted });
+      // hash the password
+      const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+      credentials.password = hash;
+
+      // save the user to the database
+      Users.add(credentials)
+          .then(user => {
+              const token = makeJwt(user);
+
+              res.status(201).json({ data: user, token });
           })
           .catch(error => {
               res.status(500).json({ message: error.message });
           });
   } else {
       res.status(400).json({
-          message: "Invalid information, plese verify and try again",
+          message: "please provide username and password and the password shoud be alphanumeric",
       });
   }
 });
 
 router.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-});
+  if (isValid(req.body)) {
+      Users.findBy({ username: username })
+          .then(([user]) => {
+              // compare the password the hash stored in the database
+              if (user && bcryptjs.compareSync(password, user.password)) {
+                  const token = makeJwt(user);
 
-router.get("/logout", (req, res) => {
-  if (req.session) {
-      req.session.destroy(err => {
-          if (err) {
-              res.status(500).json({
-                  message: "Logout Successful",
-              });
-          } else {
-              res.status(204).end();
-          }
-      });
+                  res.status(200).json({ token });
+              } else {
+                  res.status(401).json({ message: "Invalid credentials" });
+              }
+          })
+          .catch(error => {
+              res.status(500).json({ message: error.message });
+          });
   } else {
-      res.status(204).end();
+      res.status(400).json({
+          message: "please provide username and password and the password shoud be alphanumeric",
+      });
   }
 });
 
+function makeJwt({ id, username, role }) {
+  const payload = {
+      username,
+      role,
+      subject: id,
+  };
+  const config = {
+      jwtSecret: process.env.JWT_SECRET || "is it secret, is it safe?",
+  };
+  const options = {
+      expiresIn: "8 hours",
+  };
 
-function validateUser(user) {
-  // has username, password and role
-  return user.username && user.password && user.role ? true : false;
+  return jwt.sign(payload, config.jwtSecret, options);
 }
 
-function validateCredentials(creds) {
-  // has username, password and role
-  return creds.username && creds.password ? true : false;
-}
+// function validateUser(user) {
+//   // has username, password and role
+//   return user.username && user.password && user.role ? true : false;
+// }
+
+// function validateCredentials(creds) {
+//   // has username, password and role
+//   return creds.username && creds.password ? true : false;
+// }
 module.exports = router;
