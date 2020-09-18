0903e9677a19765d9bb013a638b963075bad0cdf
HernandezM-dev <HernandezM.dev@gmail.com>
2020-09-18 11:27:03 -0400

    added dependencies and created register and login requests

--- README.md
+++ README.md
@@ -25,12 +25,14 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. Differences between using _sessions_ or _JSON Web Tokens_ for authentication.
-
+    session creates a session cookie with an id that is compared with a session table on the DB
+    web token creates a token that is authenticated by the server in order to grant access to the front end client
 2. What does `bcrypt` do to help us store passwords in a secure manner.
-
+    bcrypt does a one way encrypting that can only be read by the server
 3. How are unit tests different from integration and end-to-end testing.
-
+unit tests test a particular segment of code as standalone vs which tests with the rest of the code vs end to end which tests the entire app
 4. How _Test Driven Development_ changes the way we write applications and tests.
+test driven development has us create tests that fail and we implement code in order for them to pass.
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade.
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,57 @@
 const router = require('express').Router();
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const userInfo = req.body;
+  const isValid = validateUser(userInfo);
+
+  if (isValid) {
+      // hash the password before saving the user to the database
+      const rounds = process.env.BCRYPT_ROUNDS || 4;
+      const hash = bcryptjs.hashSync(userInfo.password, rounds);
+      userInfo.password = hash;
+
+      Users.add(userInfo)
+          .then(inserted => {
+              res.status(201).json({ data: inserted });
+          })
+          .catch(error => {
+              res.status(500).json({ message: error.message });
+          });
+  } else {
+      res.status(400).json({
+          message: "Invalid information, plese verify and try again",
+      });
+  }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+
 });
 
+router.get("/logout", (req, res) => {
+  if (req.session) {
+      req.session.destroy(err => {
+          if (err) {
+              res.status(500).json({
+                  message: "Logout Successful",
+              });
+          } else {
+              res.status(204).end();
+          }
+      });
+  } else {
+      res.status(204).end();
+  }
+});
+
+
+function validateUser(user) {
+  // has username, password and role
+  return user.username && user.password && user.role ? true : false;
+}
+
+function validateCredentials(creds) {
+  // has username, password and role
+  return creds.username && creds.password ? true : false;
+}
 module.exports = router;
